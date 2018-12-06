68ea17ccbf0b863baa66f0369107a0e9a3898d29
Ondřej Sotolář <ondrej.sotolar@gmail.com>
2018-12-06 10:00:25 +0100

    Warning page update

--- zaproxy/src/org/parosproxy/paros/extension/phishingprevention/html/WarningPage.java
+++ zaproxy/src/org/parosproxy/paros/extension/phishingprevention/html/WarningPage.java
@@ -2,29 +2,27 @@ package org.parosproxy.paros.extension.phishingprevention.html;
 
 import org.parosproxy.paros.extension.phishingprevention.PasswordHygieneResult;
 
-import java.util.Collection;
-
 public class WarningPage {
     private String header = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8";
 
     private String body = "<!doctype html><html lang=\"en\"><head><meta charset=\"utf-8\">" +
             "<title>Blocked by proxy</title></head><body><H1>Blocked by proxy!</H1>" +
             "<p>Login detected! TODO: state reason...</p>" +
-            "%s%s%s</body></html>";
+            "%s%s</body></html>";
 
     private String bodyWithHygiene = "<!doctype html><html lang=\"en\"><head><meta charset=\"utf-8\">" +
             "<title>Blocked by proxy</title></head><body><H1>Blocked by proxy!</H1>" +
             "<p>Login detected! TODO: state reason...</p>" +
-            "%s%s</body></html>";
+            "%s%s%s</body></html>";
 
-    private String proceedButton = "<form action=\"http://%s\" method=\"POST\">" +
+    private String proceedButtonBase = "<form action=\"http://%s\" method=\"POST\">" +
             "<button type=\"submit\">Proceed to %s</button>" +
             "<input type=\"hidden\" name=\"save\" value=\"true\" />" +
             "<input type=\"hidden\" name=\"request_id\" value=\"%d\" />" +
             "<input type=\"hidden\" name=\"host_address\" value=\"%s\" />" +
             "</form><br>";
-    private String cancelButton = "<form action=\"http://%s\" method=\"POST\">" +
-            "<button type=\"submit\">Cancel request %s</button>" +
+    private String cancelButtonBase = "<form action=\"http://%s\" method=\"POST\">" +
+            "<button type=\"submit\">Cancel</button>" +
             "<input type=\"hidden\" name=\"save\" value=\"false\" />" +
             "<input type=\"hidden\" name=\"request_id\" value=\"%d\" />" +
             "<input type=\"hidden\" name=\"host_address\" value=\"%s\" />" +
@@ -36,23 +34,26 @@ public class WarningPage {
 
     public String getBody(int requestId, String host) {
         return String.format(body,
-                getButton(proceedButton, "localhost", requestId, host),
-                getButton(cancelButton, "localhost", requestId, host));
+                getProceedButton("localhost", requestId, host),
+                getCancelButton("localhost", requestId, host));
     }
 
     public String getBody(int requestId, String host, PasswordHygieneResult hygieneResult) {
         return String.format(bodyWithHygiene,
                 getHygieneWarningMessage(hygieneResult),
-                getButton(proceedButton, "localhost", requestId, host),
-                getButton(cancelButton, "localhost", requestId, host));
+                getProceedButton(host, requestId, host),
+                getCancelButton("localhost", requestId, host));
     }
 
     private String getHygieneWarningMessage(PasswordHygieneResult hygieneResult) {
         return "<p>Password hygiene results... TODO</p>";
     }
 
-    public String getButton(String base, String candidate, int requestId, String host) {
+    public String getProceedButton(String candidate, int requestId, String host) {
+        return String.format(proceedButtonBase, candidate, candidate, requestId, host);
+    }
 
-        return String.format(base, candidate, candidate, requestId, host);
+    public String getCancelButton(String candidate, int requestId, String host) {
+        return String.format(cancelButtonBase, candidate, requestId, host);
     }
 }
