06ab74f258650b5e1096b0a02e81167f947482d0
azalex <alexandre.arnould@student.kuleuven.be>
2018-12-11 11:18:32 +0100

    File persistence update

--- zaproxy/test/org/parosproxy/paros/extensions/phishingprevention/FilePersistenceServiceTest.java
+++ zaproxy/test/org/parosproxy/paros/extensions/phishingprevention/FilePersistenceServiceTest.java
@@ -5,10 +5,14 @@ import org.parosproxy.paros.extension.phishingprevention.Credentials;
 import org.parosproxy.paros.extension.phishingprevention.persistence.FilePersistenceService;
 
 import org.junit.Test;
+import org.parosproxy.paros.extension.phishingprevention.persistence.StoredCredentials;
+
 import java.io.File;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
@@ -26,5 +30,21 @@ public class FilePersistenceServiceTest {
         Assert.assertTrue(file.exists());
 
     }
+
+    /*public  void readFile(){
+
+        FilePersistenceService fileTest = new FilePersistenceService();
+        Credentials Creds1 = new Credentials("host", "username", "123456789");
+        Credentials Creds2 = new Credentials("alex", "azalex", "123456789");
+        Credentials Creds3 = new Credentials("dennis", "den", "123456789");
+        List<Credentials> testList = new ArrayList<>();
+
+        fileTest.setName("output.csv");
+        fileTest.saveToFile(foundCredentials,true);
+        File file = new File("src\\org\\parosproxy\\paros\\extension\\phishingprevention\\persistence\\output.csv");
+        Assert.assertTrue(file.exists());
+
+    }
+    */
 
 }
